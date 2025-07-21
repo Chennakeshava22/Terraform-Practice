91990c127683b6ccc4a30ac6089b82e35fb0216e
Chennakeshava22 <busupallechennakeshava@gmail.com>
2025-07-21 15:09:32 +0000

    environments

---
+++ environment-variables.tf
@@ -0,0 +1,30 @@
+variable "env" {}
+
+
+
+variable "kp-name" {
+  default = {
+    "dev" = "dev-kp"
+    "test" = "test-kp"
+    "prod" = "prod-kp"
+  }
+}
+
+
+variable "server-type" {
+  default = {
+    "dev" = "t2.micro"
+    "test" = "t2.medium"
+    "prod" = "t3.micro"
+  }
+}
+
+variable "insta-name" {
+  default = {
+    "dev" = "dev-server"
+    "test" = "test-server"
+    "prod" = "prod-server"
+  }
+}
+~
+
